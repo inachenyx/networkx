1be34f740da5421036a494b0fb8f2ac7a078bf65
inachenyx <89408152+inachenyx@users.noreply.github.com>
2025-05-21 12:39:40 +0800

    Added files snnTorch, main_acg, dataset

---
+++ snntorch_mnist.py
@@ -0,0 +1,37 @@
+import snntorch as snn
+from snntorch import spikeplot as splt
+from snntorch import spikegen
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+
+lif1 = snn.Leaky(beta=0.9)
+
+# dataloader
+batch_size = 128
+data_path='/tmp/data/mnist'
+# data_path='data/'
+
+dtype = torch.float
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+
+# transform
+transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0,), (1,))])
+
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+
+# create DataLoaders
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)
